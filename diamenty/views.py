6cdf152475b41a8d3184510fd979269d68f8fccc
Adam Szalowski <adamszalowski99@gmail.com>
2018-11-28 14:10:21 +0100

    Updated diamenty site

--- diamenty/views.py
+++ diamenty/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+import pickle
+import os
+from pathlib import Path
+
+home = str(Path.home())
+
+with open(home + '/kacik.txt', 'rb') as f:
+    results = pickle.load(f)
+
+def index(request):
+    return render(request, 'index.html')
+
+def onlogin(request):
+    if request.method == 'POST':
+        key = request.POST.get('secret_key', None)
+        name = results.get(key)
+        if name == None:
+            return render(request, 'error.html', {})
+        else:
+            return render(request, 'response.html', {'name' : name})
+    else:
+        return render(request, 'index.html')
 # Create your views here.
